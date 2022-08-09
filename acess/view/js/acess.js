ec438e8bc5af21c043e2faddb9956d9edb98ebed
KuroKenshi16 <igor10gondo@gmail.com>
2022-08-08 21:49:58 -0300

    Inserção do input CRO e da mascara

--- acess/view/js/acess.js
+++ acess/view/js/acess.js
@@ -10,3 +10,25 @@ btnSignin.addEventListener("click", function () {
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 });
+
+function turnVisible() {
+    //Recolhe oque está na checkbox
+    var checkBox = document.getElementById("check");
+    //Id do input
+    var text = document.getElementById("cro");
+
+    //Se a checkbox estiver selecionada mostra informação
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function cro(c) {
+    cr = c.value;
+    cr = cr.replace(/\D/g, "")
+
+    cr = cr.replace(/(\d{2})(\d{2})(\d{6})$/,"$1-$2-$3")
+    c.value = cr;
+}
