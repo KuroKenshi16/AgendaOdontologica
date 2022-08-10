9f3a90ea00274d5edb8e9228f06a37deae81d3a8
KuroKenshi16 <igor10gondo@gmail.com>
2022-08-10 20:50:51 -0300

    Correção

--- acess/view/js/acess.js
+++ acess/view/js/acess.js
@@ -11,24 +11,3 @@ btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 });
 
-function turnVisible() {
-    //Recolhe oque está na checkbox
-    var checkBox = document.getElementById("check");
-    //Id do input
-    var text = document.getElementById("cro");
-
-    //Se a checkbox estiver selecionada mostra informação
-    if (checkBox.checked == true){
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
-}
-
-function cro(c) {
-    cr = c.value;
-    cr = cr.replace(/\D/g, "")
-
-    cr = cr.replace(/(\d{2})(\d{2})(\d{6})$/,"$1-$2-$3")
-    c.value = cr;
-}
